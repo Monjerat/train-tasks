55b2b136f3f31ddbfa4bab1f7cf8dbd2e9192ffb
Monjerat <alexander.serokhvostov@gmail.com>
2023-02-08 14:17:10 +0700

    Добавлена возможность перевода из систем счисления по основанию до 36

--- radix-base-converter/main.py
+++ radix-base-converter/main.py
@@ -1,8 +1,19 @@
+def char_position(letter):
+    return ord(letter.casefold()) - 97
+
+
+def convert_radix_digit_to_int(radix_digit):
+    if radix_digit.isdigit():
+        return int(radix_digit)
+    elif radix_digit.isalpha():
+        return char_position(radix_digit) + 11
+
+
 def convert_to_decimal(base, radix_num):
     decimal_num = 0
     number_length = len(radix_num)
     for idx, digit in enumerate(radix_num):
-        decimal_num += pow(base, number_length - idx - 1) * int(digit)
+        decimal_num += pow(base, number_length - idx - 1) * convert_radix_digit_to_int(digit)
     return decimal_num
 
 
